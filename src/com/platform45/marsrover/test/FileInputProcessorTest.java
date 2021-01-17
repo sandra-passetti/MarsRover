0804a054f11f629b68770cbebafd1623a0526fb6
LAPTOP-EUTGSPOB\ljvan <sandra.passetti@gmail.com>
2021-01-17 14:26:52 +0200

    Added test cases for failure scenarios to the file input processor test

--- src/com/platform45/marsrover/test/FileInputProcessorTest.java
+++ src/com/platform45/marsrover/test/FileInputProcessorTest.java
@@ -3,7 +3,6 @@ package com.platform45.marsrover.test;
 import static org.junit.Assert.*;
 
 import com.platform45.marsrover.main.FileInputProcessor;
-import com.platform45.marsrover.main.Grid;
 import com.platform45.marsrover.main.Position;
 import com.platform45.marsrover.main.Rover;
 import org.junit.Test;
@@ -24,4 +23,23 @@ public class FileInputProcessorTest {
         assertEquals(1, rovers.get(0).getIdentifier());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testProcessFileForInvalidGrid() throws IOException {
+        FileInputProcessor.processFile("./resources/invalidGrid.txt");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testProcessFileForInvalidMovement() throws IOException {
+        FileInputProcessor.processFile("./resources/invalidMovementRequest.txt");
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testProcessFileForInvalidStartingPosition() throws IOException {
+        FileInputProcessor.processFile("./resources/invalidStartingPosition.txt");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testProcessFileForInvalidFileFormat() throws IOException {
+        FileInputProcessor.processFile("./resources/invalidFileFormat.txt");
+    }
 }
