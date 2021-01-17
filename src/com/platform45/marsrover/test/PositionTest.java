5c1e08be7a70856e224d060f4b9c697151be7f38
LAPTOP-EUTGSPOB\ljvan <sandra.passetti@gmail.com>
2021-01-17 12:25:26 +0200

    Add failing test for updating the position

---
+++ src/com/platform45/marsrover/test/PositionTest.java
@@ -0,0 +1,26 @@
+package com.platform45.marsrover.test;
+
+import com.platform45.marsrover.main.FileInputProcessor;
+import com.platform45.marsrover.main.Grid;
+import com.platform45.marsrover.main.Position;
+import com.platform45.marsrover.main.Rover;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class PositionTest {
+
+    @Test
+    public void testCalculateNewPosition() {
+        Grid grid = new Grid(5,5);
+        Position startingPosition = new Position(1,2, 'N',grid);
+        String moveCommands = "LMLMLMLMM";
+        Position newPosition = startingPosition.calculateNewPosition(moveCommands);
+        Position expectedPosition = new Position(1,3,'N',grid);
+        assertEquals(expectedPosition, newPosition);
+    }
+
+}
